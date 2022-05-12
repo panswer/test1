5b12efce56ad915d66363d3597724f141b3445d7
Ricardo Mejias <panswer.rjmm@gmail.com>
2022-05-12 18:45:37 -0400

    update: service files

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+const path = require('path');
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
+
+const pathRoot = path.resolve(__dirname, './www');
+const pathIndex = path.resolve(pathRoot, 'index.html');
+
+if (!fs.existsSync(pathIndex)) {
+    console.log("Index did't find");
+    process.exit(1);
+}
+
+app.use(express.static(pathRoot));
 
 const port = process.env.PORT || 3000;
 
